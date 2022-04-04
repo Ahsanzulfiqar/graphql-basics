aa79f22c957cdeaf89aa5da1d3b87ccae61f4d53
Ahsan Zulfiqar <mianahsan719@gmail.com>
2022-04-04 15:00:57 +0400

    push new changes

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,4 +1,32 @@
-//  generating any type of hash and private , public keys
-const  hashPassword =  "hash"
+// * hash, encryption, private & public keys
+import bcrypt from "bcrypt";
+import CryptoJS from "crypto-js";
 
-export  {hashPassword} 
+const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+const comparePassword = (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+
+const aes = (data) => {
+  try {
+    return CryptoJS.AES.encrypt(data, process.env.AES_KEY).toString();
+  } catch (error) {
+    console.log("ENCRYPTION ERROR => ", error);
+    return error;
+  }
+};
+
+const unAes = (data) => {
+  try {
+    const decrypted = CryptoJS.AES.decrypt(data, process.env.AES_KEY);
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  } catch (error) {
+    console.log("DECRYPTION ERROR => ", error);
+    return error;
+  }
+};
+
+export { hashPassword, comparePassword, aes, unAes };
