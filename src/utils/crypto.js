55872cdb97022ab087c5cba09d7c29e9bdfcce99
Ahsan Zulfiqar <mianahsan719@gmail.com>
2022-04-12 10:05:52 +0400

    crypto

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,16 +1,17 @@
 // * hash, encryption, private & public keys
 import bcrypt from "bcrypt";
 import CryptoJS from "crypto-js";
+import { ary } from "lodash";
 
 const hashPassword = async (password) => {
   return await bcrypt.hash(password, 10);
 };
 
-const comparePassword = (password, hash) => {
+const comparePassword = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 };
 
-const aes = (data) => {
+const aes = async (data) => {
   try {
     return CryptoJS.AES.encrypt(data, process.env.AES_KEY).toString();
   } catch (error) {
@@ -19,7 +20,7 @@ const aes = (data) => {
   }
 };
 
-const unAes = (data) => {
+const unAes = async (data) => {
   try {
     const decrypted = CryptoJS.AES.decrypt(data, process.env.AES_KEY);
     return decrypted.toString(CryptoJS.enc.Utf8);
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,45 +1,38 @@
 // import  {isEmail , isEmpty, equals, isAlphanumeric, isBoolean, isInt ,isJWT  ,isLength , isLowercase ,isMongoId ,isNumeric, isStrongPassword, trim } from  "validator"
-import validator from "validator"
+import validator from "validator";
 
-const {
-  isEmpty,
-  isLength,
-  isAlphanumeric,
-  isStrongPassword,
-  isEmail
-} = validator
+const { isEmpty, isLength, isAlphanumeric, isStrongPassword, isEmail } =
+  validator;
 
 const userRegistrationValidator = (params) => {
-  const {
-    name,
-    age,
-    email
-  } = params
+  const { name, age, email } = params;
   const errors = {};
 
   // * Name Validation's
   if (!isAlphanumeric(name)) {
-    errors.name = 'Name must have alphanumeric characters only.';
+    errors.name = "Name must have alphanumeric characters only.";
   }
 
-  if (!isLength(name, {
+  if (
+    !isLength(name, {
       min: 3,
-      max: 20
-    })) {
-    errors.name = 'Name must be in range of 3-20 characters.';
+      max: 20,
+    })
+  ) {
+    errors.name = "Name must be in range of 3-20 characters.";
   }
 
   // * Email Validation's
   if (!isEmail(email)) {
-    errors.email = 'Invalid Email';
+    errors.email = "Invalid Email";
   }
   if (isEmpty(email)) {
-    errors.email = 'Email Required';
+    errors.email = "Email Required";
   }
 
   // * password Validation's
   // ? let password = "joiRoot11!"
-  // ? isStrongPassword   =   minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 
+  // ? isStrongPassword   =   minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
   // if (!isStrongPassword(password)) {
   //   errors.password = 'Password must contain atleast 1 Lowercase, Uppercase, Number, Symbol and 8 character long';
   // }
@@ -51,5 +44,5 @@ const userRegistrationValidator = (params) => {
 };
 
 module.exports = {
-  userRegistrationValidator
-}
+  userRegistrationValidator,
+};
